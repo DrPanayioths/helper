3d03c76c6cd589bb48459ab3cb26abbfaea282b8
DrPanayioths <panayioths0072019@gmail.com>
2023-08-06 19:03:57 +0300

    Update: UI
    Update UI with new colors and add  support for the desktop width and heights

--- acceptmepls.js
+++ acceptmepls.js
@@ -4,4 +4,4 @@
  * Cookie That Is Automatically Created By DrPanayioths on 2 Agoust 2023 For Ownership 
  */
 document.cookie = "dataremember=Created By DrPanayioths; expires=Thu, 18 Dec 3000 01:01:01 UTC";
-
+document.cookie = "dataremember=Data Are Local Saved"
