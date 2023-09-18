98b4c3e067876707da3c9bdf5ba36310837b8baf
DrPanayioths <panayioths0072019@gmail.com>
2023-09-18 16:57:34 +0300

    Update: Added VS Code And Cookies Added
    Added New Button and Cookies For Logging Local Some Things

--- smarter.js
+++ smarter.js
@@ -1,5 +1,13 @@
+
+document.cookie = "Visited DrPanayioths Site,value=true; path=/";
+
 function thanka() {
     var boperating = prompt("What is the best operating system for you")
     var datar = boperating.toUpperCase();
     console.log(datar);
+    document.cookie = "Best Operating System for you,value=" + datar + "; path=/";
 }
+
+
+document.cookie = "download,value=true; path=/";
+
