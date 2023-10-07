bc992a878d59e9aa3a243ec2cabd33be524cdac9
Your <panayioths0072019@gmail.com>
2023-10-07 22:02:34 +0300

    Update: New Page (QR Gen)
    New Page Created For The Idea Of QR Gen

---
+++ qr.js
@@ -0,0 +1,18 @@
+function qrbuilda () {
+	var idz = document.getElementById("datra");
+	var inport = idz.value;
+	var linker = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + inport;
+
+	var pictu = document.getElementById("qrp");
+	pictu.src = linker;
+
+}	
+
+
+
+function checkEnter(event) {
+	if (event.key === "Enter") {
+		event.preventDefault();
+		qrbuilda();
+	}
+}
