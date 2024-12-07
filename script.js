7a0a37665842c55e15e32f478b31837e8546fe77
DrPanayioths <panayioths0072019@gmail.com>
2024-12-07 10:01:06 +0200

    Your IP & Country Added
    Rework Going Good, See Your IP and Country is now fully working

--- script.js
+++ script.js
@@ -2,3 +2,33 @@
 // Cookie That Is Automatically Created By DrPanayioths on 2 Agoust 2023 For Ownership 
 
 document.cookie = "dataremember=Created By DrPanayioths; expires=Thu, 18 Dec 3000 01:01:01 UTC";
+
+
+// IP Detector
+function reveal() {
+    fetch('https://api.ipify.org/?format=json')
+    .then (response => response.json())
+    .then(data => {
+      const ip = document.getElementById("ip_reveal");
+      ip.value = data.ip;
+    })
+    .catch(error => {
+        console.error('Error In Fetching The IP:', error);
+    });
+}
+  
+function hide() {
+    const ip = document.getElementById("ip_reveal");
+    ip.value = "Hover To Reveal IP";
+}
+
+// Country Display
+fetch('https://get.geojs.io/v1/ip/country.json')
+.then(response => response.json())
+.then(data => {
+    const countryname = document.getElementById('country_display');
+    countryname.value = data.name;
+})
+.catch(error => {
+    console.error('Error Fetching Country Data:', error);
+});
